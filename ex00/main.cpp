492f280c8935d866e3bea0f8c7e47a00c88769ba
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2024-01-26 09:42:06 +0200

    Rename binary, cleanup main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,9 +1,14 @@
 #include <cstdlib>
+#include <iostream>
 #include "ScalarConverter.hpp"
 
 int main(int argc, char* argv[])
 {
-    (void)argc;
+    if (argc != 2)
+    {
+        std::cout << "usage: " << argv[0] << " literal\n";
+        return EXIT_FAILURE;
+    }
     ScalarConverter::convert(argv[1]);
     return EXIT_SUCCESS;
 }
