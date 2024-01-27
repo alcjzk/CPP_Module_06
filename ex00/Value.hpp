436b5c541f6924ace4c84985e581ce1592752aa3
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2024-01-27 13:26:01 +0200

    Simplified template

--- ex00/Value.hpp
+++ ex00/Value.hpp
@@ -22,44 +22,38 @@ class Value
         std::optional<T> as_optional() const
         {
             return std::visit([](auto value) {
-                return option_from<T>(value);
+                return optional_from<T>(value);
             }, _value);
         }
 
     private:
         std::variant<std::monostate, char, int, float, double> _value;
 
+        template <typename T, typename U>
+        static std::optional<T> optional_from(U value)
+        {
+            if constexpr (std::is_integral_v<T> && std::is_floating_point_v<U>)
+            {
+                U integer_part;
+                if (std::isnan(value) ||
+                    std::isinf(value) ||
+                    std::modf(value, &integer_part) != 0.0)
+                {
+                    return std::nullopt;
+                }
+            }
+            if (std::isfinite(value) &&
+               (value < std::numeric_limits<T>::lowest() ||
+                value > std::numeric_limits<T>::max()))
+            {
+                return std::nullopt;
+            }
+            return std::optional<T>(static_cast<T>(value));
+        }
+
         template <typename T>
         static std::optional<T> option_from(std::monostate)
         {
             return std::nullopt;
-        }
-
-        template <typename T, typename U, std::enable_if_t<
-            std::is_floating_point_v<U> && std::is_integral_v<T>, bool> = true>
-        static std::optional<T> option_from(U value)
-        {
-            U integer_part;
-
-            if (std::isnan(value) ||
-                value < std::numeric_limits<T>::min() ||
-                value > std::numeric_limits<T>::max() ||
-                std::modf(value, &integer_part) != 0.0)
-            {
-                return std::nullopt;
-            }
-            return std::optional<T>(static_cast<T>(value));
-        }
-
-        template <typename T, typename U, std::enable_if_t<
-            !std::is_floating_point_v<U> || std::is_floating_point_v<T>, bool> = true>
-        static std::optional<T> option_from(U value)
-        {
-            if (value < std::numeric_limits<T>::min() ||
-                value > std::numeric_limits<T>::max())
-            {
-                return std::nullopt;
-            }
-            return std::optional<T>(static_cast<T>(value));
         }
 };
