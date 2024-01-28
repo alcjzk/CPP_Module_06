12c8316eab59bcd4f8f742e02409743d5746debf
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2024-01-28 11:56:52 +0200

    Added simple tests

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,6 +1,21 @@
 #include <cstdlib>
+#include <cassert>
+#include <iostream>
+#include "Data.hpp"
+#include "Serializer.hpp"
 
 int main()
 {
+    Data* data = new Data(42);
+
+    assert(data->value() == 42);
+
+    Data* data_copy = Serializer::deserialize(Serializer::serialize(data));
+    assert(data == data_copy);
+
+    assert(data_copy->value() == 42);
+
+    std::cout << "Tests ok\n";
+
     return EXIT_SUCCESS;
 }
